9e3735515000ddb5efff0b1e1999164f39576818
Md. Emon Miah <md.emonmiahisthebest@gmail.com>
2024-11-29 12:41:00 +0600

    final update and next update comming soon after payment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3842658789"
+    serviceWorkerVersion: "3220701524"
   }
 });
