88f85365165bb1a93341c902821dfd91c55395d6
MD Emon Miah <mdemonmiah@MDs-MacBook-Pro.local>
2024-12-03 16:37:01 +0600

    update app bar scroll, forgot screen, add address, app error solve

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,23 @@
-// importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
-// importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js");
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCHSzk4n1LhTnDyv64u6cKldJMq5k_NDOs",
-    authDomain: "ecom-metrix.firebaseapp.com",
-    projectId: "ecom-metrix",
-    storageBucket: "ecom-metrix.firebasestorage.app",
-    messagingSenderId: "972064786517",
-    appId: "1:972064786517:web:c26ba23af86ccbe02e2cdc",
-    measurementId: "G-KZB78LK8VE"
-  };
 
-firebase.initializeApp(firebaseConfig);
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD760iA7Dka5gbn8EfS8FPgU7X6uAql6tE",
+  authDomain: "brand-and-more-86768.firebaseapp.com",
+  projectId: "brand-and-more-86768",
+  storageBucket: "brand-and-more-86768.firebasestorage.app",
+  messagingSenderId: "55448858846",
+  appId: "1:55448858846:web:2fc7f0610589dc174e124f",
+  measurementId: "G-Y098Y18H5M"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const messaging = firebase.messaging();
 
